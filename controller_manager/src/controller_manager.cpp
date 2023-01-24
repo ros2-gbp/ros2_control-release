540b5fd989ce708886f237fb2e1ae9436a175fdd
Bence Magyar <bence.magyar.robotics@gmail.com>
2023-01-24 18:40:28 +0000

    Rebase from 'upstream'

--- controller_manager/src/controller_manager.cpp
+++ controller_manager/src/controller_manager.cpp
@@ -178,6 +178,10 @@ ControllerManager::ControllerManager(
     std::make_shared<pluginlib::ClassLoader<controller_interface::ChainableControllerInterface>>(
       kControllerInterfaceNamespace, kChainableControllerInterfaceClassName))
 {
+  if (!get_parameter("update_rate", update_rate_))
+  {
+    RCLCPP_WARN(get_logger(), "'update_rate' parameter not set, using default value.");
+  }
   diagnostics_updater_.setHardwareID("ros2_control");
   diagnostics_updater_.add(
     "Controllers Activity", this, &ControllerManager::controller_activity_diagnostic_callback);
@@ -309,7 +313,7 @@ controller_interface::ControllerInterfaceBaseSharedPtr ControllerManager::load_c
       controller = chainable_loader_->createSharedInstance(controller_type);
     }
   }
-  catch (const pluginlib::CreateClassException e)
+  catch (const pluginlib::CreateClassException & e)
   {
     RCLCPP_ERROR(
       get_logger(), "Error happened during creation of controller '%s' with type '%s':\n%s",
