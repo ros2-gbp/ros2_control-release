ab493afba3fb0419caa7ca8b44911512ff72e30a
Bence Magyar <bence.magyar.robotics@gmail.com>
2024-12-06 10:19:33 +0000

    Rebase from 'debian/jazzy/hardware_interface'

--- src/component_parser.cpp
+++ src/component_parser.cpp
@@ -58,6 +58,7 @@ constexpr const auto kTypeAttribute = "type";
 constexpr const auto kRoleAttribute = "role";
 constexpr const auto kReductionAttribute = "mechanical_reduction";
 constexpr const auto kOffsetAttribute = "offset";
+constexpr const auto kReadWriteRateAttribute = "rw_rate";
 constexpr const auto kIsAsyncAttribute = "is_async";
 
 }  // namespace
@@ -220,6 +221,42 @@ std::string parse_data_type_attribute(const tinyxml2::XMLElement * elem)
   }
 
   return data_type;
+}
+
+/// Parse rw_rate attribute
+/**
+ * Parses an XMLElement and returns the value of the rw_rate attribute.
+ * Defaults to 0 if not specified.
+ *
+ * \param[in] elem XMLElement that has the rw_rate attribute.
+ * \return unsigned int specifying the read/write rate.
+ */
+unsigned int parse_rw_rate_attribute(const tinyxml2::XMLElement * elem)
+{
+  const tinyxml2::XMLAttribute * attr = elem->FindAttribute(kReadWriteRateAttribute);
+  try
+  {
+    const auto rw_rate = attr ? std::stoi(attr->Value()) : 0;
+    if (rw_rate < 0)
+    {
+      throw std::runtime_error(
+        "Could not parse rw_rate tag in \"" + std::string(elem->Name()) + "\"." + "Got \"" +
+        std::to_string(rw_rate) + "\", but expected a positive integer.");
+    }
+    return static_cast<unsigned int>(rw_rate);
+  }
+  catch (const std::invalid_argument & e)
+  {
+    throw std::runtime_error(
+      "Could not parse rw_rate tag in \"" + std::string(elem->Name()) + "\"." +
+      " Invalid value : \"" + attr->Value() + "\", expected a positive integer.");
+  }
+  catch (const std::out_of_range & e)
+  {
+    throw std::runtime_error(
+      "Could not parse rw_rate tag in \"" + std::string(elem->Name()) + "\"." +
+      " Out of range value : \"" + attr->Value() + "\", expected a positive valid integer.");
+  }
 }
 
 /// Parse is_async attribute
@@ -575,6 +612,7 @@ HardwareInfo parse_resource_from_xml(
   HardwareInfo hardware;
   hardware.name = get_attribute_value(ros2_control_it, kNameAttribute, kROS2ControlTag);
   hardware.type = get_attribute_value(ros2_control_it, kTypeAttribute, kROS2ControlTag);
+  hardware.rw_rate = parse_rw_rate_attribute(ros2_control_it);
   hardware.is_async = parse_is_async_attribute(ros2_control_it);
 
   // Parse everything under ros2_control tag
@@ -942,6 +980,22 @@ std::vector<InterfaceDescription> parse_state_interface_descriptions(
   return component_state_interface_descriptions;
 }
 
+void parse_state_interface_descriptions(
+  const std::vector<ComponentInfo> & component_info,
+  std::unordered_map<std::string, InterfaceDescription> & state_interfaces_map)
+{
+  state_interfaces_map.reserve(state_interfaces_map.size() + component_info.size());
+
+  for (const auto & component : component_info)
+  {
+    for (const auto & state_interface : component.state_interfaces)
+    {
+      InterfaceDescription description(component.name, state_interface);
+      state_interfaces_map.insert(std::make_pair(description.get_name(), description));
+    }
+  }
+}
+
 std::vector<InterfaceDescription> parse_command_interface_descriptions(
   const std::vector<ComponentInfo> & component_info)
 {
@@ -957,6 +1011,22 @@ std::vector<InterfaceDescription> parse_command_interface_descriptions(
     }
   }
   return component_command_interface_descriptions;
+}
+
+void parse_command_interface_descriptions(
+  const std::vector<ComponentInfo> & component_info,
+  std::unordered_map<std::string, InterfaceDescription> & command_interfaces_map)
+{
+  command_interfaces_map.reserve(command_interfaces_map.size() + component_info.size());
+
+  for (const auto & component : component_info)
+  {
+    for (const auto & command_interface : component.command_interfaces)
+    {
+      InterfaceDescription description(component.name, command_interface);
+      command_interfaces_map.insert(std::make_pair(description.get_name(), description));
+    }
+  }
 }
 
 }  // namespace hardware_interface
