1697ae9a5ace11d7665199ab0331203f2b70bc48
Bence Magyar <bence.magyar.robotics@gmail.com>
2024-12-29 20:28:40 +0000

    Rebase from 'upstream'

--- hardware_interface/include/hardware_interface/types/trigger_type.hpp
+++ hardware_interface/include/hardware_interface/types/trigger_type.hpp
@@ -1,4 +1,4 @@
-// Copyright 2023 ros2_control Development Team
+// Copyright 2024 ros2_control Development Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,23 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef HARDWARE_INTERFACE__LEXICAL_CASTS_HPP_
-#define HARDWARE_INTERFACE__LEXICAL_CASTS_HPP_
-
-#include <string>
+#ifndef HARDWARE_INTERFACE__TYPES__TRIGGER_TYPE_HPP_
+#define HARDWARE_INTERFACE__TYPES__TRIGGER_TYPE_HPP_
 
 namespace hardware_interface
 {
-
-/** \brief Helper function to convert a std::string to double in a locale-independent way.
- \throws std::invalid_argument if not a valid number
- * from
- https://github.com/ros-planning/srdfdom/blob/ad17b8d25812f752c397a6011cec64aeff090c46/src/model.cpp#L53
-*/
-double stod(const std::string & s);
-
-bool parse_bool(const std::string & bool_string);
+enum class TriggerType
+{
+  READ,
+  WRITE
+};
 
 }  // namespace hardware_interface
 
-#endif  // HARDWARE_INTERFACE__LEXICAL_CASTS_HPP_
+#endif  // HARDWARE_INTERFACE__TYPES__TRIGGER_TYPE_HPP_
