08fd807c1412487b59603add90ac8fc30243293f
Bence Magyar <bence.magyar.robotics@gmail.com>
2025-07-21 13:52:29 +0100

    Rebase from 'upstream'

--- hardware_interface/include/hardware_interface/sensor.hpp
+++ hardware_interface/include/hardware_interface/sensor.hpp
@@ -45,12 +45,27 @@ public:
 
   ~Sensor() = default;
 
+  Sensor(const Sensor & other) = delete;
+
+  Sensor & operator=(const Sensor & other) = delete;
+
+  Sensor & operator=(Sensor && other) = delete;
+
+  [[deprecated(
+    "Replaced by const rclcpp_lifecycle::State & initialize(const "
+    "hardware_interface::HardwareComponentParams & params).")]]
   const rclcpp_lifecycle::State & initialize(
     const HardwareInfo & sensor_info, rclcpp::Logger logger,
     rclcpp::node_interfaces::NodeClockInterface::SharedPtr clock_interface);
 
+  [[deprecated(
+    "Replaced by const rclcpp_lifecycle::State & initialize(const "
+    "hardware_interface::HardwareComponentParams & params).")]]
   const rclcpp_lifecycle::State & initialize(
     const HardwareInfo & sensor_info, rclcpp::Logger logger, rclcpp::Clock::SharedPtr clock);
+
+  const rclcpp_lifecycle::State & initialize(
+    const hardware_interface::HardwareComponentParams & params);
 
   const rclcpp_lifecycle::State & configure();
 
