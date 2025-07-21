e99ee3669dff196853d758d2e40404af8e1d62a6
Bence Magyar <bence.magyar.robotics@gmail.com>
2025-07-21 13:52:32 +0100

    Rebase from 'upstream'

--- hardware_interface/include/hardware_interface/actuator.hpp
+++ hardware_interface/include/hardware_interface/actuator.hpp
@@ -45,12 +45,27 @@ public:
 
   ~Actuator() = default;
 
+  Actuator(const Actuator & other) = delete;
+
+  Actuator & operator=(const Actuator & other) = delete;
+
+  Actuator & operator=(Actuator && other) = delete;
+
+  [[deprecated(
+    "Replaced by const rclcpp_lifecycle::State & initialize(const "
+    "hardware_interface::HardwareComponentParams & params).")]]
   const rclcpp_lifecycle::State & initialize(
     const HardwareInfo & actuator_info, rclcpp::Logger logger,
     rclcpp::node_interfaces::NodeClockInterface::SharedPtr clock_interface);
 
+  [[deprecated(
+    "Replaced by const rclcpp_lifecycle::State & initialize(const "
+    "hardware_interface::HardwareComponentParams & params).")]]
   const rclcpp_lifecycle::State & initialize(
     const HardwareInfo & actuator_info, rclcpp::Logger logger, rclcpp::Clock::SharedPtr clock);
+
+  const rclcpp_lifecycle::State & initialize(
+    const hardware_interface::HardwareComponentParams & params);
 
   const rclcpp_lifecycle::State & configure();
 
